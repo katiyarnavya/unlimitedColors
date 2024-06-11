47c8337b14f0b17a68a32b7f9046c9131fc2ae91
katiyarnavya <148884485+katiyarnavya@users.noreply.github.com>
2024-06-11 21:07:38 +0530

    unlimited colors

---
+++ one.js
@@ -0,0 +1,16 @@
+const random_color = function(){
+    const hex = '0123456789ABCDEF';
+    let color = '#';
+    for(let i = 0; i < 6; i++){
+        color += hex[Math.floor(Math.random()*16)];
+    }
+    return color;
+}
+const startChangingColor = function(){
+    document.body.style.backgroundColor = random_color();
+}
+const stopChangingColor = function(){
+   
+}
+document.querySelector('#start').addEventListener('click', startChangingColor);
+document.querySelector('#stop').addEventListener('click', stopChangingColor);
